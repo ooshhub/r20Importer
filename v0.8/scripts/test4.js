cc6d87ee9bbfec21c1c203121b0acd4e55af9414
ooshhub <74662220+ooshhub@users.noreply.github.com>
2021-04-04 16:33:56 +0930

    Delete v0.8 directory

--- v0.8/scripts/test4.js
+++
@@ -1,7 +0,0 @@
-const floppy = async () => {
-	let bob = new Promise(res => setTimeout(() => res(console.log('flop')), 3000));
-	let alice = new Promise(res => setTimeout(() => res(console.log('faecal')), 5000));
-	await Promise.all([alice, bob]).then(() => console.log('poot'))
-}
-
-floppy()
